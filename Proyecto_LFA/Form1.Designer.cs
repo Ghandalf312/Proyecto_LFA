d45cb098f6c6a01686f9ce3b2d534ca5510d3fa8
Ghandalf312 <djmoralesm@correo.url.edu.gt>
2022-05-06 20:06:48 -0600

    Add project files.

---
+++ Proyecto_LFA/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿
+namespace Proyecto_LFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtInicial = new System.Windows.Forms.RichTextBox();
+            this.txtEstados = new System.Windows.Forms.RichTextBox();
+            this.txtFinales = new System.Windows.Forms.RichTextBox();
+            this.txtRecorridos = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtTransiciones = new System.Windows.Forms.RichTextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtPila = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Seleccionar archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(23, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 117);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Para cargar el automata, haga clic en el botón \"Seleccionar archivo\", y seleccion" +
+    "e el archivo .txt con el debido formato.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Proyecto_LFA.Properties.Resources.GANDALF;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.ImageLocation = "";
+            this.pictureBox2.Location = new System.Drawing.Point(899, 353);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(151, 166);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Proyecto_LFA.Properties.Resources.zero;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(718, 353);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 166);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(708, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "José De León - 1170419";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(884, 330);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Diego Morales - 1132119";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(774, 224);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 7;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(243, 155);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(193, 173);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Automata cargado";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtInicial);
+            this.groupBox1.Controls.Add(this.txtEstados);
+            this.groupBox1.Controls.Add(this.txtFinales);
+            this.groupBox1.Controls.Add(this.txtRecorridos);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(17, 334);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(686, 185);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pruebas";
+            // 
+            // txtInicial
+            // 
+            this.txtInicial.Location = new System.Drawing.Point(6, 112);
+            this.txtInicial.Name = "txtInicial";
+            this.txtInicial.Size = new System.Drawing.Size(205, 33);
+            this.txtInicial.TabIndex = 7;
+            this.txtInicial.Text = "";
+            // 
+            // txtEstados
+            // 
+            this.txtEstados.Location = new System.Drawing.Point(6, 42);
+            this.txtEstados.Name = "txtEstados";
+            this.txtEstados.Size = new System.Drawing.Size(205, 33);
+            this.txtEstados.TabIndex = 6;
+            this.txtEstados.Text = "";
+            // 
+            // txtFinales
+            // 
+            this.txtFinales.Location = new System.Drawing.Point(226, 42);
+            this.txtFinales.Name = "txtFinales";
+            this.txtFinales.Size = new System.Drawing.Size(205, 33);
+            this.txtFinales.TabIndex = 5;
+            this.txtFinales.Text = "";
+            // 
+            // txtRecorridos
+            // 
+            this.txtRecorridos.Location = new System.Drawing.Point(463, 42);
+            this.txtRecorridos.Name = "txtRecorridos";
+            this.txtRecorridos.Size = new System.Drawing.Size(216, 137);
+            this.txtRecorridos.TabIndex = 4;
+            this.txtRecorridos.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(463, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 20);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Recorridos";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(226, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Estados finales";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 89);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Estado inicial";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Cantidad estados";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(918, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Evaluar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(328, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(430, 42);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Lenguajes Formales y Autómatas";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(347, 51);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(384, 46);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Implementación de Automata de Pila";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(792, 174);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(191, 36);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Evaluación de cadenas";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtTransiciones
+            // 
+            this.txtTransiciones.Location = new System.Drawing.Point(442, 155);
+            this.txtTransiciones.Name = "txtTransiciones";
+            this.txtTransiciones.Size = new System.Drawing.Size(193, 173);
+            this.txtTransiciones.TabIndex = 16;
+            this.txtTransiciones.Text = "";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(442, 132);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(138, 20);
+            this.label12.TabIndex = 17;
+            this.label12.Text = "Transiciones hechas";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(641, 132);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(33, 20);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "Pila";
+            // 
+            // txtPila
+            // 
+            this.txtPila.Location = new System.Drawing.Point(641, 155);
+            this.txtPila.Name = "txtPila";
+            this.txtPila.Size = new System.Drawing.Size(62, 173);
+            this.txtPila.TabIndex = 18;
+            this.txtPila.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 542);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.txtPila);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtTransiciones);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Automata de pila";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox txtInicial;
+        private System.Windows.Forms.RichTextBox txtEstados;
+        private System.Windows.Forms.RichTextBox txtFinales;
+        private System.Windows.Forms.RichTextBox txtRecorridos;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.RichTextBox txtTransiciones;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.RichTextBox txtPila;
+    }
+}
+
---
+++ Proyecto_LFA/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Proyecto_LFA
+{
+    public partial class Form1 : Form
+    {
+        public string inicial;
+        public string[] finales;
+        public Stack Pila = new Stack();
+        public string estadoActual;
+        public List<List<string>> estados;
+        public string transicionHecha;
+        public string pilaResultante;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Proceso de la carag de archivo
+                string ruta;
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                string acepto = Convert.ToString(openFileDialog.ShowDialog());
+                ruta = openFileDialog.FileName;
+
+                if (acepto == "OK") { } else { ruta = "";}
+            
+                TextReader Leer_archivo = new StreamReader(ruta);
+               
+                richTextBox1.Text = Leer_archivo.ReadToEnd();
+    
+                //Variables para particionar el archivo de texto
+                string archivo = richTextBox1.Text;
+                string[] separador = archivo.Split('\n');
+                string aux;
+                string total;
+                string recorridos = string.Empty;
+                estados = new List<List<string>>();
+                string[] transicion;
+
+                txtRecorridos.Text = Convert.ToString(separador);
+                total = separador[0];
+                inicial = separador[1];
+                aux = separador[2];
+                finales = aux.Split(",");
+
+                // Creacion de estados con base al archivo de texto
+                for (int i = 0; i < int.Parse(total); i++)
+                {
+                    List<string> nuevoEstado = new List<string>();
+                    estados.Add(nuevoEstado);
+                }
+                // Insercion de los recorridos que puede tener el estado
+                for (int i = 3; i < separador.Length; i++)
+                {
+                    recorridos = recorridos + separador[i] + "\n";
+                    transicion = separador[i].Split(",");
+                    estados[int.Parse(transicion[0])].Add(separador[i]);
+                }
+
+                txtEstados.Text = total;
+                txtInicial.Text = inicial;
+                txtFinales.Text = aux;
+                txtRecorridos.Text = recorridos;
+                txtTransiciones.Text = "";
+                txtPila.Text = "";
+
+                button2.Enabled = true;
+                textBox1.Enabled = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ruta o Archivo no valido.", "Error");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            textBox1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Variables para evaluar la cadena entrante
+            Pila = new Stack();
+            int tamanioCadena = 0;
+            int cantidadTransiciones = 0;
+            bool cadenaValida = true;
+            bool encontrado = false;
+            string[] transicion;
+
+            // Se empieza a evaluar la cadena
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    transicionHecha = "";
+                    pilaResultante = "";
+                    estadoActual = inicial;
+                    while (tamanioCadena < textBox1.Text.Length && cadenaValida == true)
+                    {
+                        encontrado = false;
+                        cantidadTransiciones = 0;
+                        while (encontrado == false && estados[int.Parse(estadoActual)].Count > cantidadTransiciones)
+                        {
+                            transicion = estados[int.Parse(estadoActual)][cantidadTransiciones].Split(",");
+                            if (textBox1.Text[tamanioCadena].ToString() == transicion[1])
+                            {
+                                if (transicion[2] != "")
+                                {
+                                    Pila.Pop();
+                                }
+                                if (transicion[3] != "")
+                                {
+                                    if (transicion[3].ToString().Length > 1)
+                                    {
+                                        for (int i = 0; i < transicion[3].ToString().Length; i++)
+                                        {
+                                            char[] separador = transicion[3].ToCharArray();
+                                            Pila.Push(separador[i].ToString());
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Pila.Push(transicion[3].ToString());
+                                    }
+                                }
+                                estadoActual = transicion[4];
+                                encontrado = true;
+                                for (int i = 0; i < transicion.Length; i++)
+                                {
+                                    if (transicion[i] == "")
+                                    {
+                                        transicion[i] = "ε";
+                                    }
+                                }
+                                transicionHecha = transicionHecha + "→ ( ( " + transicion[0]+", "+transicion[1]+", "+transicion[2]+" ), ( "+transicion[4]+", "+transicion[3]+" ) )" + "\n";
+                            }
+                            cantidadTransiciones++;
+
+                            // No encontro el caracter a leer en los posbiles caracteres del estado
+                            if (encontrado == false && estados[int.Parse(estadoActual)].Count == cantidadTransiciones)
+                            {
+                                cadenaValida = false;
+                            }
+                        }
+                        tamanioCadena++;
+
+                    }
+                    var aux = Pila.ToArray();
+                    pilaResultante = String.Join("\n", aux);
+                    if (pilaResultante == "")
+                    {
+                        pilaResultante = pilaResultante + "ε";
+                    }
+                    foreach (var estadoFinal in finales)
+                    {
+                        if (estadoActual == estadoFinal)
+                        {
+                            if (cadenaValida == true && Pila.Count == 0)
+                            {
+                                txtPila.Text = pilaResultante;
+                                txtTransiciones.Text = transicionHecha;
+                                MessageBox.Show("La cadena es valida.");
+                            }
+                            else
+                            {
+                                txtPila.Text = pilaResultante;
+                                txtTransiciones.Text = transicionHecha;
+                                MessageBox.Show("La cadena no es valida.");
+                            }
+                        }
+                        else
+                        {
+                            txtPila.Text = pilaResultante;
+                            txtTransiciones.Text = transicionHecha;
+                            MessageBox.Show("La cadena no es valida.");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Debe ingresarse texto para poder validar.");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("La cadena no es valida, pila vacía.");
+            }   
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label10_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
